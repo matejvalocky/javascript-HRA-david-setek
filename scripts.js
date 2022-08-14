1f996b91ed243f83babd0f21762a82fa77a69723
Matej Valocký <74997007+matejvalocky@users.noreply.github.com>
2022-08-14 22:27:35 +0200

    pridanie celkoveho skore

--- scripts.js
+++ scripts.js
@@ -1,3 +1,5 @@
-// 58. Ovládni JavaScript - Hra v JavaScriptu nový projekt a stažení obrázků kostky
+//58. Ovládni JavaScript - Hra v JavaScriptu nový projekt a stažení obrázků kostky
 
-// 59. Ovládni JavaScript - Hra v JavaScriptu: pozadí linear gradient a hrací plocha
+//59. Ovládni JavaScript - Hra v JavaScriptu: pozadí linear gradient a hrací plocha
+
+//60. Ovládni JavaScript - Hra v JavaScriptu: přidání celkového skóre
