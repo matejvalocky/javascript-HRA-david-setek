c7e6c849e1369c73cd3096e0743c623efa0c58ec
Matej Valocký <74997007+matejvalocky@users.noreply.github.com>
2022-08-15 18:13:59 +0200

    oprava total score druheho hraca

--- scripts.js
+++ scripts.js
@@ -3,3 +3,5 @@
 //59. Ovládni JavaScript - Hra v JavaScriptu: pozadí linear gradient a hrací plocha
 
 //60. Ovládni JavaScript - Hra v JavaScriptu: přidání celkového skóre
+
+//61. Ovládni JavaScript - Hra v JavaScriptu: přidání současného skóre
